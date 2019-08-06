df0e582e6dc0613d8db616d0bff077b1b3bd67d0
AlexAnt84 <53814566+AlexAnt84@users.noreply.github.com>
2019-08-06 11:43:32 +0300

    Create VKparser.py

---
+++ VKparser.py
@@ -0,0 +1,13 @@
+﻿import requests
+import time
+from lxml import html
+
+list = open('D:\Downloads\парс\likes.txt', encoding='UTF-8') #Список id
+parsed_list = open('D:\Downloads\парс\parsed_1.txt','w',encoding='UTF-8') #Место для списка id и пользователей
+
+for line in list:
+    #time.sleep(3)
+    response = requests.get(line.rstrip('\r\n'))    
+    parsed=html.fromstring(response.text)
+    parsed_list.write(line.rstrip('\r\n') + ' | ' + parsed.xpath('//title/text()')[0] + '\n')
+    
